4c9bc59907952ce842653a6cc83250b9e64d7a41
Lena-vetochka <elenanechaeva1701@yandex.ru>
2024-08-17 23:15:27 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+
+immutable_var = 1, 0.5, 'hi', False #кортеж
+print(immutable_var)
+
+
+mutable_listn = [1, 0.5, 'hi', False] #список
+print(mutable_listn)
+mutable_listn.append('bye')
+mutable_listn.extend('c+')
+mutable_listn.remove (False)
+mutable_listn [-1] = '-'
+print(mutable_listn)
